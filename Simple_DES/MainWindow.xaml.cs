2d668c4a5be39cd6ee31b5390bf11f1c63b40ba3
Do CB93u <BorsukRA@gmail.com>
2024-10-21 01:23:15 +0300

    Гауна дабавил х2

--- Simple_DES/MainWindow.xaml.cs
+++ Simple_DES/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace Simple_DES
     public partial class MainWindow : Window
     {
         private char[] data;
-        private int key;
+        private static int key;
         public MainWindow()
         {
             InitializeComponent();
@@ -31,7 +31,7 @@ namespace Simple_DES
             data = Default_TextBox.Text.ToCharArray();
             try
             {
-                int.TryParse(Default_TextBox.Text, out key);
+                int.TryParse(Key_TextBox.Text, out key);
             } catch(Exception ex) { MessageBox.Show("Произошла ошибка: " + ex.Message); }
                 
 
@@ -54,7 +54,9 @@ namespace Simple_DES
 
     public static class S_DES
     {
-        private static int[] P10_arr = { 2,4,1,6,3,9,0,8,7,5 }; //Перестановка Р10: { 3 5 2 7 4 10 1 9 8 6 }
+        private readonly static int[] P10_arr = { 2 ,4 ,1 ,6 ,3 ,9 ,0 ,8 ,7 ,5 }; //Перестановка Р10: { 3 5 2 7 4 10 1 9 8 6 }
+        private readonly static int[] P8_arr = { 5, 2, 6, 3, 7, 4, 9, 8 }; //Перестановка Р8: { 6 3 7 4 8 5 10 9 - - }
+        private readonly static int[] IP_arr = { 1, 5, 4, 0, 3, 7, 4, 6 }; //Перестановка IP: { 2 6 3 1 4 8 5 7 }
         public static string CharToBitString(char ch) //потом убрать
         {
             byte[] bytes = Encoding.ASCII.GetBytes(new char[] { ch });
@@ -80,8 +82,14 @@ namespace Simple_DES
         {
             byte[] bytes = BitConverter.GetBytes(value);
             BitArray bitArray = new BitArray(bytes);
+            BitArray temp_bitArray = new BitArray(10);
 
-            return bitArray;
+            for(int i = 0; i < 10; i++)
+            {
+                temp_bitArray[i] = bitArray[i];
+            }
+
+            return temp_bitArray;
         }
 
         public static string BitToString(BitArray bitArray)
@@ -101,10 +109,31 @@ namespace Simple_DES
             int length = bitArray.Length;
             BitArray bitArray_temp = new BitArray(length);
 
-            for(int i = 0; i < 10;  i++)
+            for(int i = 0; i < length;  i++)
             {
                 bitArray_temp[i] = bitArray[P10_arr[i]];  
             }
+
+            return bitArray_temp;
+        }
+
+        public static BitArray P8(BitArray bitArray)
+        {
+            int length = bitArray.Length;
+            BitArray bitArray_temp = new BitArray(length - 2);
+
+            for(int i = 0; i < length - 2; i++)
+            {
+                bitArray_temp[i] = bitArray[P8_arr[i]];
+            }
+
+            return bitArray_temp;
+        }
+
+        public static BitArray IP(BitArray bitArray)
+        {
+            int length = bitArray.Length;
+            BitArray bitArray_temp = new BitArray(length - 2);
 
             return bitArray_temp;
         }
@@ -113,17 +142,35 @@ namespace Simple_DES
         {
             int length = bitArray.Length;
             BitArray bitArray_temp = new BitArray(length);
+            int halfLength = length / 2;
 
-            bool firstBit = bitArray[0];
+            if (length % 2 != 0)
+            {
+                throw new ArgumentException("BitArray length must be even.");
+            }
 
-            for (int i = 1; i < length; i++)
+            // Циклический сдвиг первой половины
+            bool firstBitFirstHalf = bitArray[0];
+            for (int i = 1; i < halfLength; i++)
             {
                 bitArray_temp[i - 1] = bitArray[i];
             }
+            bitArray_temp[halfLength - 1] = firstBitFirstHalf;
 
-            bitArray_temp[length - 1] = firstBit;
+            // Циклический сдвиг второй половины
+            bool firstBitSecondHalf = bitArray[halfLength];
+            for (int i = halfLength + 1; i < length; i++)
+            {
+                bitArray_temp[i - 1] = bitArray[i];
+            }
+            bitArray_temp[length - 1] = firstBitSecondHalf;
+/*
+            for (int i = 0; i < length; i++)
+            {
+                MessageBox.Show(bitArray_temp[i].ToString());
+            }*/
 
-            return bitArray_temp;
+            return bitArray_temp; // Возвращаем измененный массив
         }
 
         public static BitArray Reverse(BitArray bitArray)
@@ -131,10 +178,13 @@ namespace Simple_DES
             int length = bitArray.Length;
             BitArray bitArray_temp = new BitArray(length);
 
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < length; i++)
             {
                 bitArray_temp[i] = bitArray[length - 1 - i];
             }
+
+
+
             return bitArray_temp;
         }
 
@@ -143,14 +193,32 @@ namespace Simple_DES
             BitArray bitArray_char = new BitArray(8);
             BitArray bitArray_key = new BitArray(10);
 
+            //Подготовка массива битов для финальных ключей, юзнем больше памяти чем дозволено, но повысим читабельность 
+            BitArray bitArray_key_final1 = new BitArray(8);
+            BitArray bitArray_key_final2 = new BitArray(8);
+
+            //Подготовка массива битов к работе
             bitArray_key = IntToBit(key);
             bitArray_key = Reverse(bitArray_key);
+            
+            //Перестановка Р10
+            bitArray_key = P10(bitArray_key);
+            //Циклический сдвиг влево
+            bitArray_key = CSlideLeft(bitArray_key);
+            //Перестановка Р8
+            bitArray_key_final1 = P8(bitArray_key);
+
+            //Двойной циклический сдвиг влево
+            bitArray_key = CSlideLeft(bitArray_key);
+            bitArray_key = CSlideLeft(bitArray_key);
+            //Перестановка Р8
+            bitArray_key_final2 = P8(bitArray_key);
+
+            //Подготовка массива битов к работе
             bitArray_char = CharToBit(ch);
             bitArray_char = Reverse(bitArray_char);
 
-            bitArray_key = CSlideLeft(bitArray_key);
-     
-            return BitToString(bitArray_char);
+            return BitToString(bitArray_key_final2);
         }
     }
 
